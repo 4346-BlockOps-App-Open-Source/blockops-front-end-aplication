622fd7bb5360897cc929347226c03dd154db19f9
RenzoVi21 <renzovillata@gmail.com>
2025-05-16 07:57:00 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,30 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './features/landing/landing.component';
-
+import { RegisterComponent } from './features/register/register.component';
+import { RiskProfileComponent } from './features/risk-profile/risk-profile.component';
+import { PreferencesComponent } from './features/preferences/preferences.component';
+import { EducacionfComponent } from './features/educacionf/educacionf.component';
 export const routes: Routes = [
   {
     path: '',
     component: LandingComponent,
     title: 'Landing Page'
   },
-  // Aquí puedes añadir nuevas rutas conforme vayas agregando más funcionalidades
-  // Ejemplo:
-  // {
-  //   path: 'about',
-  //   component: AboutComponent
-  // },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Registro de Usuario'
+  },
+  {
+    path: 'risk-profile',
+    component: RiskProfileComponent,
+    title: 'Selección de Perfil de Riesgo'
+  },
+  {
+    path: 'preferences',
+    component: PreferencesComponent,
+    title: 'Personalización de Señales'
+  },
+  { path: '', component: LandingComponent },
+  { path: 'educacion-financiera', component: EducacionfComponent }
 ];
